52a6730516d366dfbe8176e43761435710f09419
SazzFixon <sazztheone@gmail.com>
2024-10-25 09:15:01 +0300

    Debugged and ready to use e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :petimg="petimg" />
+</template>
+
+<script>
+import Vue from 'vue'
+import petimg from '@petimg/core'
+import { Dashboard } from '@petimg/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    petimg: () => new petimg(),
+  },
+}
+</script>
+
+<style src="@petimg/core/dist/style.css"></style>
+<style src="@petimg/dashboard/dist/style.css"></style>
